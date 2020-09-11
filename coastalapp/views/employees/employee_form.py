dbe7186ccf377c5cbbd64b73cd0de9a59e20f1d4
Adrian Garmendia <dejima@gmail.com>
2020-09-11 15:34:21 -0500

    created employee views

---
+++ coastalapp/views/employees/employee_form.py
@@ -0,0 +1,51 @@
+import sqlite3
+from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from coastalapp.models import Department
+from coastalapp.models import Employee
+from ..connection import Connection
+from ..employees.employee_details import get_employee
+
+
+def get_departments():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            d.id,
+            d.department_name,
+        from coastalapp_department d
+        """)
+
+        return db_cursor.fetchall()
+
+def employee_form(request):
+    if request.method == 'GET':
+        departments = get_departments()
+        template = 'employees/employee_form.html'
+        context = {
+            'all_departments': departments
+        }
+
+        return render(request, template, context)
+
+def employee_edit_form(request, employee_id):
+
+    if request.method == 'GET':
+        employee = get_employee(employee_id)
+        departments = get_departments()
+
+        template = 'employees/employee_form.html'
+        context = {
+            'employee': employee,
+            'all_departments': departments
+        }
+
+        return render(request, template, context)
+             
+
+        
