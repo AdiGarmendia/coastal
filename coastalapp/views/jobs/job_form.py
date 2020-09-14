96fbd3c79e5d73f760c1f82da2d97cbe2dc3c890
Adrian Garmendia <dejima@gmail.com>
2020-09-14 10:32:03 -0500

    finished adding views and the imports

--- coastalapp/views/jobs/job_form.py
+++ coastalapp/views/jobs/job_form.py
@@ -1,17 +1,17 @@
 import sqlite3
 from django.shortcuts import render
-from coastalapp.models import TrainingProgram
+from coastalapp.models import Job
 from .job_details import get_job
 from ..connection import Connection
 
-def training_form(request):
+def job_form(request):
     if request.method == 'GET':
         template = 'jobs/job_form.html'
 
         return render(request, template)
 
 
-def training_edit_form(request, job_id):
+def job_edit_form(request, job_id):
 
     if request.method == "GET":
         job = get_job(job_id)
