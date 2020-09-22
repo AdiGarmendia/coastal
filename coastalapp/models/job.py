ed03c68ccfe07d68494997d653d73f4aa1e68eed
Adrian Garmendia <dejima@gmail.com>
2020-09-22 15:16:18 -0500

    refactored jobs to use a joined table when assigned an employee

--- coastalapp/models/job.py
+++ coastalapp/models/job.py
@@ -1,6 +1,5 @@
 from django.db import models
-from .employee import Employee
-
+from django.shortcuts import reverse
 
 class Job(models.Model):
 
@@ -8,13 +7,12 @@ class Job(models.Model):
     title = models.CharField(max_length=100)
     start_date = models.DateField(null=True, auto_now=False, auto_now_add=False)
     end_date = models.DateField(null=True, auto_now=False, auto_now_add=False)
-    assigned_employee = models.ForeignKey(Employee,
-                                   related_name="assigned_employees",
-                                   null=True,  # Makes column nullable in DB
-                                   blank=True,  # Allows blank value on objects
-                                   on_delete=models.CASCADE)
+    
     
 
     class Meta:
         verbose_name = ("Job")
         verbose_name_plural = ("Jobs")
+
+    def get_absolute_url(self):
+        return reverse("job_details", kwargs={"pk": self.pk})
